aa828b9740e45313782441d08908e30c66d769d3
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-09-28 00:22:52 +0300

    initializes the project with components

---
+++ src/componets/addForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddBook = () => {
+    return (
+        <form>
+            <input type="text" placeholder="Add book"></input>
+            <input type="submit" value="Add"></input>
+        </form>
+    )
+}
+
+export default AddBook
---
+++ src/componets/books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AddBook from "./addForm";
+
+const Books = () => {
+    return(
+        <>
+            <h1>My Books</h1>
+            <AddBook/>
+        </>
+    )
+}
+
+export default Books

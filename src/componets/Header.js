705ae74e89cc2901eb5db3809a6af14c2fbad92a
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-10-01 18:30:19 +0300

    adds styles to the header

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../styles/App.css';
+import { FaUser } from 'react-icons/fa';
+import '../styles/Header.css';
 
 const Header = () => {
   const links = [
-    {
-      id: 1,
-      path: '/categories',
-      text: 'Categories',
-    },
+
     {
       id: 2,
       path: '/',
       text: 'Books',
+    },
+    {
+      id: 1,
+      path: '/categories',
+      text: 'Categories',
     },
 
   ];
   return (
     <header className="header">
       <div className="HeaderLeft">
-        <h3>BookStore</h3>
+        <h1 className="bookStoreHeading">BookStore</h1>
+
+        <ul>
+          {links.map((link) => (
+            <li key={link.id}>
+              <NavLink to={link.path} activeClassName="active-link" exact>{link.text}</NavLink>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul className="headerRight">
-        {links.map((link) => (
-          <li key={link.id}>
-            <NavLink to={link.path} activeClassName="active-link" exact>{link.text}</NavLink>
-          </li>
-        ))}
-      </ul>
+      <div className="headerRight"><FaUser /></div>
     </header>
   );
 };
